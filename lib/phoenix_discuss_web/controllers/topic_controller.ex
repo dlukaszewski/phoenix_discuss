297ff3146394fbe401b8e1d1905a70bc8e7574cc
dlukaszewski <lukaszewsidamian@gmail.com>
2021-03-17 13:23:38 +0100

    creating a new form commit

--- lib/phoenix_discuss_web/controllers/topic_controller.ex
+++ lib/phoenix_discuss_web/controllers/topic_controller.ex
@@ -1,9 +1,15 @@
-defmodule PhoenixDiscuss.TopicController do
+defmodule PhoenixDiscussWeb.TopicController do
    use PhoenixDiscussWeb, :controller
    
     alias PhoenixDiscuss.Topic
 
-    def new(conn, params) do
+    def new(conn, _params) do
         changeset = Topic.changeset(%Topic{}, %{})
+
+        render(conn, "new.html", changeset: changeset)
+    end
+
+    def create(conn, %{"topic" => topic}) do
+        
     end
 end
